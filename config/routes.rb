5069ee0306c7abfe3e88cadab9cf16674e2f12ca
Mateus Veloso <mfvelosos@gmail.com>
2019-06-17 01:11:46 -0300

    Add new routes to coordinates and localization

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
 
-      get 'complains/search', to: 'complains#search'
+      get 'complains/search',       to: 'complains#search'
+      get 'complains/coordinates',  to: 'complains#coordinates'
+      get 'complains/:id/localization', to: 'complains#localization'
       resources :complains, only: [:index, :show, :create]
 
     end
