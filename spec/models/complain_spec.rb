5483fef5d3258f69a3468bfa5c86eccd31570fed
Mateus Veloso <mfvelosos@gmail.com>
2019-06-17 12:07:38 -0300

    Create complain model test

--- spec/models/complain_spec.rb
+++ spec/models/complain_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Complain, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'valid without name' do
+    expect(Complain.create(
+      title: 'titulo Teste',
+      description: 'descrição',
+      locale: 'são paulo, sp',
+      company: 'empresa teste'
+    )).to be_valid
+  end
+
+  it 'invalid without title' do
+    expect(Complain.create(
+      description: 'descrição',
+      locale: 'são paulo, sp',
+      company: 'empresa teste'
+    )).to_not be_valid
+  end
+
+  it 'invalid without description' do
+    expect(Complain.create(
+      title: 'titulo Teste',
+      locale: 'são paulo, sp',
+      company: 'empresa teste'
+    )).to_not be_valid
+  end
+
+  it 'invalid without locale' do
+    expect(Complain.create(
+      title: 'titulo Teste',
+      description: 'descrição',
+      company: 'empresa teste'
+    )).to_not be_valid
+  end
+
+  it 'invalid without company' do
+    expect(Complain.create(
+      title: 'titulo Teste',
+      description: 'descrição',
+      locale: 'são paulo, sp'
+    )).to_not be_valid
+  end
+
+  it 'check coordinates presence after save' do
+    complain = Complain.create(
+                              name: 'Nome teste',
+                              title: 'titulo Teste',
+                              description: 'descrição',
+                              locale: 'são paulo, sp',
+                              company: 'empresa teste'
+                              )
+    expect(complain.coordinates).to be_truthy
+  end
+
+  it 'check if coodinates array is valid' do
+    complain = Complain.create(
+                              name: 'Nome teste',
+                              title: 'titulo Teste',
+                              description: 'descrição',
+                              locale: 'são paulo, sp',
+                              company: 'empresa teste'
+                              )
+    expect(complain.coordinates.length).to eq(2)
+  end
+
 end
