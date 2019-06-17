983cb405c0e8ef8505aa480bb6593dca49978bab
Mateus Veloso <mfvelosos@gmail.com>
2019-06-17 00:09:15 -0300

    Setting complains routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+
+      get 'complains/search', to: 'complains#search'
       resources :complains, only: [:index, :show, :create]
+
     end
   end
 end
