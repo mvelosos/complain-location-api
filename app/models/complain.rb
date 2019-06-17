4b56e63522ce9932a1e97b016c91ec45c5ea838e
Mateus Veloso <mfvelosos@gmail.com>
2019-06-17 00:08:56 -0300

    Add name attribute and mongo modules

--- app/models/complain.rb
+++ app/models/complain.rb
@@ -1,7 +1,16 @@
 class Complain
   include Mongoid::Document
+  include Mongoid::Timestamps
+  include Geocoder::Model::Mongoid
+  # include Geocoder::Model::MongoMapper
+
+  field :name, type: String
   field :title, type: String
   field :description, type: String
   field :locale, type: String
   field :company, type: String
+  field :coordinates, type: Array
+
+  geocoded_by :locale
+  after_validation :geocode
 end
